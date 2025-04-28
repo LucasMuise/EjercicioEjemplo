6f0e7774021b07c44995ea35636984420801eafe
lucasmuise <lucassmuise@gmail.com>
2025-04-28 20:58:55 -0300

    prueba

---
+++ Persona.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    class Persona
+    {      //CONSTRUCTORES
+        public Persona(string nombre) {
+       this.nombre = nombre;
+        }
+        private string nombre;
+        private int edad;
+        private string nacionalidad;
+
+        //propiedades
+        public string Nombre
+        {
+            get { return nombre; }
+        } 
+     
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+
+        }
+        public string Nacionalidad
+        {
+            get { return nacionalidad; }
+            set { nacionalidad = value; }
+        }
+        //Métodos
+        
+        public string saludar()
+        {
+            return "Hola soy " + nombre;
+        }
+
+        //Sobrecarga de métodos
+
+        public string saludar(string nombre)
+        {
+            return "hola  " + nombre + ".. soy : " + this.nombre;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Lucas");
+          
+
+            Console.WriteLine("ingrese su edad..");
+            p1.Edad= int.Parse( Console.ReadLine() );
+            Console.WriteLine("ingrese su nacionalidad..");
+            p1.Nacionalidad = Console.ReadLine();
+
+
+            Console.WriteLine("El nombre de la persona es : " + p1.Nombre);
+
+            Console.WriteLine("su edad es : " + p1.Edad + " años ");
+
+            Console.WriteLine("La nacionalidad es: " + p1.Nacionalidad);
+
+            Console.WriteLine(p1.saludar());
+            string nombre;
+            Console.WriteLine("ingrese nombre de la persona : ");
+            nombre =Console.ReadLine();
+            Console.WriteLine(p1.saludar(nombre));
+
+            Console.ReadKey();  
+        }
+    }
+}
